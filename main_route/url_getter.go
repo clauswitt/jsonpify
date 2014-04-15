4d00019c24d95235d9f150adb2247d816ba54da4
Claus Witt <claus@wittnezz.dk>
2014-04-15 21:03:57 +0200

    added inline caching with memcached

--- main_route/url_getter.go
+++ main_route/url_getter.go
@@ -4,7 +4,13 @@ import (
   "fmt"
   "bytes"
   "net/http"
+  "github.com/bradfitz/gomemcache/memcache"
+  "os"
+  "crypto/md5"
+  "io"
 )
+
+var mc *memcache.Client
 
 func getUrl(url string) bytes.Buffer {
   response, err := http.Get( url )
@@ -18,7 +24,30 @@ func getUrl(url string) bytes.Buffer {
   return *buf
 }
 
+
 func wrapContentFromUrl(url, wrapper string) string {
-  content := getUrl(url)
+  mc = memcache.New(os.Getenv("MEMCACHE_SERVERS"))
+  h := md5.New()
+  io.WriteString(h, url)
+  md5_url := fmt.Sprintf("%x", h.Sum(nil))
+
+  var content bytes.Buffer
+
+  it, err := mc.Get(md5_url)
+
+
+  if err == nil && it != nil {
+    buf := new(bytes.Buffer)
+    buf.Write(it.Value)
+    content = *buf
+  } else {
+    content = getUrl(url)
+    item := &memcache.Item{Key: md5_url, Value: []byte(content.Bytes())}
+    mc.Set(item)
+    buf := new(bytes.Buffer)
+    buf.Write(item.Value)
+    content = *buf
+  }
+
   return fmt.Sprintf(wrapper, content.String())
 }
